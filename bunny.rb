2360ff91277aa8da58bde1327b0cca403fb862e1
Matthew Hall <hallmatthew314@gmail.com>
2018-11-09 21:38:11 +1300

    Impliment random bunny names based on gender

--- bunny.rb
+++ bunny.rb
@@ -6,8 +6,9 @@ class Bunny
 		@age = 0
 		@sex = rand(2) # 1 or 0, male or female
 		@colour = rand(4) # 0..3, corresponds to a constant in colour.rb
-		@name = "Name Machine Broke" # TODO: Create a pool of names to randomly select from
 		@rmvb = rand(50) == 0 # @rmvb is true 2% of the time upon creation
+		namelist = @sex == 0 ? 'names_female.txt' : 'names_male.txt' # Pick name pool based on this bunny's sex
+		@name = File.readlines(namelist)[rand(50)].strip # Pick a name from the name pool and strip whitespace
 	end
 
 	def to_s
