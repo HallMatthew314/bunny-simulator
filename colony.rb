8aa4def26dcf46258d57fca6f7c44207ff40be1c
Matthew Hall <hallmg1@student.op.ac.nz>
2018-11-10 14:08:42 +1300

    Create next_year method

--- colony.rb
+++ colony.rb
@@ -4,9 +4,17 @@ class Colony
 
 	def initialize()
 		@year = 0
-		@bunnies = Array.new(5) { Bunny.new }
 
-		#test
+		next_year() until @year > 0 && @bunnies.empty?
+	end
+
+	def next_year()
+		@year += 1
 		EventLogger.announce_year_start(@year)
+
+		@bunnies = Array.new(5) { Bunny.new } if year == 1
+
+		puts "Press enter for next year"
+		gets
 	end
 end
