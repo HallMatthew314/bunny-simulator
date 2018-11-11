3619ce9f8a6291bf2c491e235ca2a595b5dd7f2a
Matthew Hall <hallmg1@student.op.ac.nz>
2018-11-11 13:15:37 +1300

    Allow bunnies to be created with a given colour

--- bunny.rb
+++ bunny.rb
@@ -2,10 +2,11 @@ class Bunny
   
   attr_accessor :name, :age, :sex, :colour, :rmvb
 
-  def initialize(*args)
+  def initialize(colour: 'random')
     @age = 0
     @sex = rand(2) # 1 or 0, male or female
-    @colour = rand(4) # 0..3, corresponds to a constant in colour.rb
+    @colour = colour # If colour is not specified (as int), randomly select one on the next line
+    @colour = rand(4) if @colour == 'random' # 0..3, corresponds to a constant in colour.rb
     @rmvb = rand(50) == 0 # @rmvb is true 2% of the time upon creation
     namelist = @sex == 0 ? 'names_female.txt' : 'names_male.txt' # Pick name pool based on this bunny's sex
     @name = File.readlines(namelist)[rand(50)].strip # Pick a name from the name pool and strip whitespace
