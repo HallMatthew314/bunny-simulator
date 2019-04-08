e5ab64e9c24a77000c98e1d419ab407ea6c53535
Matthew Hall <hallmg1@student.op.ac.nz>
2019-04-08 21:31:12 +1200

    Implement log files

--- Ruby/event_logger.rb
+++ Ruby/event_logger.rb
@@ -1,27 +1,42 @@
 class EventLogger
   
+  def self.set_filename
+    @@filename = format("bunny_sim_log_%s.txt", Time.now.to_i)
+  end
+
+  def self.output(message)
+    puts message
+    File.open(@@filename, 'a') { |f| f.write(message + "\n") }
+  end
+
   def self.announce_year_start(year)
-    puts format("-- START OF YEAR %s --", year)
+    message = format("-- START OF YEAR %s --", year)
+    EventLogger.output(message)
   end
 
   def self.announce_birth(name, rmvb)
     # Refer to a bunny as either 'Bunny' or 'Radioactive Mutant Vampire Bunny'
-    puts format("%sBunny %s was born!", (rmvb == true ? 'Radioactive Mutant Vampire ' : ''), name)
+    message = format("%sBunny %s was born!", (rmvb == true ? 'Radioactive Mutant Vampire ' : ''), name)
+    EventLogger.output(message)
   end
 
   def self.announce_older(name, rmvb, age)
-    puts format("%sBunny %s is now %s year%s old", (rmvb == true ? 'Radioactive Mutant Vampire ' : ''), name, age, (age == 1 ? '' : 's'))
+    message = format("%sBunny %s is now %s year%s old", (rmvb == true ? 'Radioactive Mutant Vampire ' : ''), name, age, (age == 1 ? '' : 's'))
+    EventLogger.output(message)
   end
 
   def self.announce_death(name, rmvb, age, cause: CauseOfDeath::OLD_AGE)
-    puts format("%sBunny %s has died of %s (age %s)", (rmvb == true ? 'Radioactive Mutant Vampire ' : ''), name, CauseOfDeath.value_to_string(cause), age)
+    message = format("%sBunny %s has died of %s (age %s)", (rmvb == true ? 'Radioactive Mutant Vampire ' : ''), name, CauseOfDeath.value_to_string(cause), age)
+    EventLogger.output(message)
   end
 
   def self.announce_infection(name)
-    puts format("Bunny %s was infected!", name)
+    message = format("Bunny %s was infected!", name)
+    EventLogger.output(message)
   end
 
   def self.announce_famine(population)
-    puts format("The colony's population has reached %s, resulting in a food shortage.", population)
+    message = format("The colony's population has reached %s, resulting in a food shortage.", population)
+    EventLogger.output(message)
   end
 end
--- Ruby/main.rb
+++ Ruby/main.rb
@@ -5,5 +5,7 @@ load 'cause_of_death.rb'
 load 'bunny.rb'
 load 'colony.rb'
 
+# Calculate unix timestamp for output filename
+EventLogger.set_filename
 # Begin the simulation
-c = Colony.new
+Colony.new
